86e5866aaa3bdd9885059aff0edcaf8b38e8abef
jonpas <jonpas33@gmail.com>
2015-01-22 20:28:17 +0100

    Fixed #8

--- @JNS_Skycranes/mod.cpp
+++ @JNS_Skycranes/mod.cpp
@@ -1,3 +1,5 @@
 name = "JNS Skycranes 1.0.0";
+picture = "jns_skycranes_logo.paa";
 actionName = "GitHub";
 action = "https://github.com/jonpas/A3_JNS_Skycranes";
+overview = "JNS Skycranes puts the Mi-290 Taru helicopters and it's pods to the BLUFOR side.";
